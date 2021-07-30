2c899a01cc78e31069b8044da3184e68c3d8cb71
ChaseTheCoder <chasefsheaff@gmail.com>
2021-07-29 22:45:24 -0500

    feat: basic pages created with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './Pages/Home';
+import About from './Pages/About';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/about" exact component={About}/>
         </Switch>
       </Router>
     </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link className="navbar-logo">
-          TakeABreak
-        </Link>
-      </div>
+      <ul className="navbar-menu">
+        <li>
+          <Link className="navbar-logo navbar-link">
+            <i class="fas fa-hourglass-half"></i>
+            TakeABreak
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="navbar-link">
+            About
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
