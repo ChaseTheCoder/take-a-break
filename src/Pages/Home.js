ee58fd2fec43bbe5dc627dd545ca698be3bfb87e
ChaseTheCoder <chasefsheaff@gmail.com>
2021-08-08 23:56:14 -0500

    style: add home page initial style

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,7 +10,7 @@ function Home() {
           <p className="home-text-p">Learning to identify your feelings daily will help with communicating those feelings.  Students can check in with this app when they have uncomfortable feelings.</p>
           <p className="home-text-p-green">No signup required!</p>
         </div>
-        <button>Start</button>
+        <button className="start-button">Start</button>
       </div>
       <div className="home-right">
         <i className="fas fa-hourglass-half"></i>
