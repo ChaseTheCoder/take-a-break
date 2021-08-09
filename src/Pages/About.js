873a82f56341522f1b228a4329bab99f8594b804
ChaseTheCoder <chasefsheaff@gmail.com>
2021-08-09 10:37:21 -0500

    feat: add about page with video

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './About.css'
 
 function About() {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className="about">
+      <div className="about-left">
+        <div className="about-text">
+          <h1 className="about-text-title">Instead of just taking a break, TakeABreak.</h1>
+          <p className="about-text-p">TakeABreak allows a student to identify their emotion selecting an emoji.  Then it will bring them to a video where they can learn about that emotion.  Taking a break is now more purposeful.</p>
+          <p className="about-text-p">Taking a break allows children time for reflecting on past experiences, and planning for the future.
+Breaks throughout the school day can be an effective way to reduce disruptive behavior</p>
+        </div>
+      </div>
+      <div className="about-right">
+      <video src="./videos/child-meditate.mp4" autoPlay loop muted />
+      </div>
     </div>
   )
 };
